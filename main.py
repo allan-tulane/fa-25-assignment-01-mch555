00e9cde54b6c2ca88ee836d7ac5aaea83ceffc0b
Maeren Hay <148922167+mch555@users.noreply.github.com>
2025-09-15 13:08:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,22 @@ See assignment-01.pdf for details.
 # no imports needed.
 
 def foo(x):
-    ### TODO
-    pass
+    if x<=1:
+        return x
+    else:
+        return foo(x-1) + foo(x-2)
+
 
 def longest_run(mylist, key):
-    ### TODO
-    pass
+    current = 0
+    longest =0
+    for num in mylist:
+        if num==key:
+            current+=1
+            longest=max(longest, current)
+        else:
+            current = 0 
+    return longest
 
 
 class Result:
@@ -37,8 +47,33 @@ def to_value(v):
         return int(v)
         
 def longest_run_recursive(mylist, key):
-    ### TODO
-    pass
+    if len(mylist)==0:
+        return Result(0, 0, 0, True)
+    if len(mylist)==1:
+        if mylist[0] == key:
+            return Result(1, 1, 1, True)
+        else:
+            return Result(0, 0, 0, False)
+    
+    mid = len(mylist)//2
+    left = longest_run_recursive(mylist[:mid], key)
+    right = longest_run_recursive(mylist[mid:], key)
+
+    is_entire_range= left.is_entire_range and right.is_entire_range
+    
+    if left.is_entire_range:
+        left_size = left.left_size + right.left_size
+    else:
+        left_size = left.left_size
+
+    if right.is_entire_range:
+        right_size = right.right_size + left.right_size
+    else:
+        right_size = right.right_size
+    
+    crossing = left.right_size + right.left_size
+    longest_size = max(left.longest_size, right.longest_size, crossing)
+    return Result(left_size, right_size, longest_size, is_entire_range)
 
 
 
